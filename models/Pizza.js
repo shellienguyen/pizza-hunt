362d67260dfc1de76f0b157675818c0e8a19f07e
sidonie <shelliednnguyen@gmail.com>
2021-04-14 18:32:20 -0700

    data validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,14 +2,19 @@ const { Schema, model } = require( 'mongoose' );
 const dateFormat = require( '../utils/dateFormat' );
 
 const PizzaSchema = new Schema({
-   pizzaName: { type: String },
-   createdBy: { type: String },
+   pizzaName: { type: String, required: true, trim: true },
+   createdBy: { type: String, required: true, trim: true },
    createdAt: {
       type: Date,
       default: Date.now,
       get: ( createdAtVal ) => dateFormat( createdAtVal )
-    },
-   size: { type: String, default: 'Large' },
+   },
+   size: { 
+      type: String,
+      required: true,
+      enum: [ 'Personal', 'Small', 'Medium', 'Large', 'Extra Large' ],
+      default: 'Large'
+   },
    toppings: [],
    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]},
    {
